b79dcd4cb54197ec68ae478584204fee7d783f87
Eriq Fields <eriqfields@MacBook-Pro.localdomain>
2021-10-14 19:25:21 -0700

    inital commit

---
+++ birdbrainpython/lesson_1.py
@@ -0,0 +1,11 @@
+from BirdBrain import Finch
+bird = Finch()
+
+
+
+print("eriqexercise")
+bird.setMove("F",120,70)
+bird.setTurn("R",90,10)
+bird.setMove("F",360,80)
+bird.setTurn("L",90,10)
+bird.setMove("F",1000,60)
